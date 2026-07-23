19b68bd4f04da9bc7bfa677ef7867509e154b4e9
orderly-guoqing <guoqing@orderly.network>
2026-07-23 22:02:14 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -20,7 +20,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Portfolio,Markets,Leaderboard,Swap,Points,Trading",
   "VITE_CUSTOM_MENUS": "Tetreum DApp, https://dapp.tetreum.com/; Cross Chain Bridge, https://bridge.tetreum.com/; Join Presale, https://dapp.tetreum.com/register",
-  "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
+  "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "true",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "https://t.me/tetreum",
   "VITE_DISCORD_URL": "",
